a1b16f0e23926e3a8a6ca15b492e6a8dff0193c5
Spote777 <66115092+Spote777@users.noreply.github.com>
2020-07-16 23:43:34 +0300

    Lesson5 (#3)
    * Задание к четвертому уроку
    
    * Задание к пятому уроку
    
    * мелкие исправленя

---
+++ Урок 5/5|_ЗаруцковПавел.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+import PlaygroundSupport
+import Foundation
+
+protocol Car {
+    var openWindows: Bool { get }
+    var startedEngine: Bool { get }
+}
+
+extension Car {
+    func windows() {
+        if openWindows == true {
+            print("Окна открыты")
+        } else {
+            print("Окна закрыты")
+        }
+    }
+    func engine(){
+        if startedEngine == true {
+            print("Машина заведена")
+        } else {
+            print("Машина заглушена")
+        }
+    }
+}
+
+class SportCar: Car {
+    var openWindows: Bool
+    var startedEngine: Bool
+    var maxSpeedPerSecond: Double
+    
+    init(openWindows: Bool, startedEngine: Bool, maxSpeedPerSecond: Double) {
+        self.openWindows = openWindows
+        self.startedEngine = startedEngine
+        self.maxSpeedPerSecond = maxSpeedPerSecond
+    }
+}
+extension SportCar: CustomStringConvertible {
+    var description: String {
+        return "(\(maxSpeedPerSecond))"
+    }
+    
+    
+}
+
+class TrunkCar: Car {
+    var openWindows: Bool
+    var startedEngine: Bool
+    var trunkVolume: Double
+    
+    init(openWindows: Bool, startedEngine: Bool, trunkVolume: Double) {
+        self.openWindows = openWindows
+        self.startedEngine = startedEngine
+        self.trunkVolume = trunkVolume
+    }
+}
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "объем: \(trunkVolume)"
+    }
+}
+var car1 = SportCar(openWindows: true, startedEngine: true, maxSpeedPerSecond: 280)
+print(car1.maxSpeedPerSecond)
+var car2 = TrunkCar(openWindows: false, startedEngine: true, trunkVolume: 20)
+print(car2)
